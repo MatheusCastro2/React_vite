5c4ebf638852fa574ea76a0f5efac0403bed8d06
MatheusCastro <matheusnc2004@gmail.com>
2025-03-27 20:24:27 -0300

    atualizacao da imagem

--- codeconnct/src/App.jsx
+++ codeconnct/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 import BarraPesquisa from './componentes/BarraPesquisa'
+import Card from './componentes/Card'
+import Filtro from './componentes/Filtro'
 import SideBar from './componentes/SideBar'
+import './componentes/Card'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes')
+    .then(response => response.json())
+    .then(dados => setDados(dados))
+  }, [])  
 
   return (
     <div className='container'>
         <SideBar />
-        <BarraPesquisa />
+        <div>
+          <BarraPesquisa />
+          <Filtro />
+          <Card />
+        </div>
     </div>
 
   )
